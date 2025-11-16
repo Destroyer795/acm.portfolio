b0a4136e9113fadea33f24cb0696ce7585049115
Karthik Saiharsh <ks.the.visionary@gmail.com>
2025-11-16 16:29:58 +0530

    Added details of Core Team

--- src/routes/team/index.tsx
+++ src/routes/team/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Card from "@/components/team/Card";
-import { Faculty } from "@/lib/team-details";
+import { CoreTeam, Faculty } from "@/lib/team-details";
 
 export const Route = createFileRoute("/team/")({
 	component: RouteComponent,
@@ -8,14 +8,36 @@ export const Route = createFileRoute("/team/")({
 
 function RouteComponent() {
 	return (
-		<section className="flex flex-col justify-center items-center gap-5">
-			<section className="w-full h-fit pt-10 flex flex-col items-center justify-center">
+		<section className="flex flex-col justify-center items-center gap-5 py-15">
+			<section className="w-full h-fit flex flex-col items-center justify-center">
 				<section className="flex flex-col justify-center items-center gap-7">
 					<p className="text-3xl font-sans font-bold">Our Guides & Mentors</p>
 
 					<div className="grid grid-cols-3 justify-center items-center gap-7">
-						{Faculty.map((faculty, idx) => (
-							<Card key={idx} person={faculty} type="large" />
+						{Faculty.map((faculty) => (
+							<Card
+								key={faculty.name}
+								person={faculty}
+								type="large"
+								category="faculty"
+							/>
+						))}
+					</div>
+				</section>
+			</section>
+
+			<section className="w-full h-fit pt-10 flex flex-col items-center justify-center">
+				<section className="flex flex-col justify-center items-center gap-7">
+					<p className="text-3xl font-sans font-bold">Core Team</p>
+
+					<div className="grid grid-cols-4 justify-center items-center gap-7">
+						{CoreTeam.map((coreteam) => (
+							<Card
+								key={coreteam.name}
+								person={coreteam}
+								type="large"
+								category="student"
+							/>
 						))}
 					</div>
 				</section>
