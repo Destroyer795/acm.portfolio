40a38d3c2a204dd0b419d9da88482549bd12bc0f
Destroyer795 <ty.pranavkishan7905@gmail.com>
2025-11-16 15:53:46 +0530

    refactor: Removed tanstack devtools and edited website metadata

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,6 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { TanStackDevtools } from "@tanstack/react-devtools";
-
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export const Route = createRootRoute({
   component: () => (
@@ -13,17 +10,6 @@ export const Route = createRootRoute({
         <Outlet />
       </div>
       <Footer />
-      <TanStackDevtools
-        config={{
-          position: "bottom-right",
-        }}
-        plugins={[
-          {
-            name: "Tanstack Router",
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
     </div>
   ),
-});
+})
